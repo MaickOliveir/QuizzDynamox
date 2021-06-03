497707a0b88c14c289f1c1ac64e8b0af4d449535
mckgeol2014@gmail.com <MaTiMa04@>
2021-06-03 19:25:58 -0300

    first commit

---
+++ app/src/main/java/com/mkgeol/learningproject/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mkgeol.learningproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
